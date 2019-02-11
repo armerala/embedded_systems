d1fe7d75d120a2a4b702a7bb0bd22efbf609e1b1
Daniel Mesko <dpmesko@gmail.com>
2019-02-11 15:57:00 -0500

    implemented hex-7seg decoder

--- lab1/lab1.sv
+++ lab1/lab1.sv
@@ -50,8 +50,26 @@ endmodule
 module hex7seg(input logic [3:0] a,
 	       output logic [6:0] y);
 
-   assign y = {a[2:0],a}; // Replace this with your code
-   
+   always_comb
+     case (a)
+		4'd0:	y = ~7'b111_1110;
+		4'd1:	y = ~7'b011_0000;
+		4'd2:	y = ~7'b110_1101;
+		4'd3:	y = ~7'b111_1001;
+		4'd4:	y = ~7'b011_0011;
+		4'd5:	y = ~7'b101_1011;
+		4'd6:	y = ~7'b101_1111;
+		4'd7:	y = ~7'b111_0000;
+		4'd8:	y = ~7'b111_1111;
+		4'd9:	y = ~7'b111_1011;
+		4'd10:	y = ~7'b111_0111;
+		4'd11:	y = ~7'b001_1111;
+		4'd12:	y = ~7'b100_1110;
+		4'd13:	y = ~7'b011_1101;
+		4'd14:	y = ~7'b100_1111;
+		4'd15:	y = ~7'b100_0111;
+		default: y = ~7'b000_0000;
+	  endcase 
 endmodule
 
 // 16 X 8 synchronous RAM with old data read-during-write behavior
