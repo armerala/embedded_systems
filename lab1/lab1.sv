bd3b329b44cdf48164b27c946c42ebfa2d713326
cabukc <cansucabuk@gmail.com>
2019-02-12 00:15:45 -0500

    Added 7-segment display

--- lab1/lab1.sv
+++ lab1/lab1.sv
@@ -80,7 +80,26 @@ endmodule
 module hex7seg(input logic [3:0] a,
 	       output logic [6:0] y);
 
-   assign y = {a[2:0],a}; // Replace this with your code
+	always_comb
+		case (a)
+			4'd0:		y = 7'h40;		//0
+			4'd1:		y = 7'h79;  	//1
+			4'd2:		y = 7'h24;  	//2
+			4'd3:		y = 7'h30;		//3
+			4'd4:		y = 7'h19;  	//4
+			4'd5:		y = 7'h12;  	//5
+			4'd6:		y = 7'h02;  	//6
+			4'd7:		y = 7'h78;		//7 	 
+			4'd8:		y = 7'h00;		//8
+			4'd9:		y = 7'h10;		//9		
+			4'd10:		y = 7'h08;		//A
+			4'd11:		y = 7'h03;		//b		
+			4'd12:		y = 7'h46;		//C
+			4'd13:		y = 7'h21;		//d
+			4'd14:		y = 7'h06;		//E
+			4'd15:		y = 7'h0E;		//F
+			default:	y = 7'h40;		//0
+		endcase
    
 endmodule
 
