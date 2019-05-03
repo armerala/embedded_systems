dbb07decb5d7ca7f72dca7cbcc100468ae2bc0d0
Daniel Mesko <dpmesko@gmail.com>
2019-05-03 18:07:42 +0000

    more work on decoding USB gamepad packets

--- project/software/gamepad_driver/gamepad.c
+++ project/software/gamepad_driver/gamepad.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+#include <linux/joystick.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "usbkeyboard.h"
 
@@ -9,50 +11,71 @@ uint8_t endpoint_address;
 
 struct usb_keyboard_packet packet;
 
+void using_libusb();
+void using_joystick();
+
+
 int main()
+{
+
+//	using_joystick();
+
+	using_libusb();
+	return 0;
+}
+
+
+void using_joystick()
+{
+		printf("%s\n", JSIOCGVERSION);
+}
+
+
+
+void using_libusb()
 {
 	int transferred;
 
 	keyboard = openkeyboard(&endpoint_address);
 
-	unsigned char buf[8], oldbuf[8];
+	unsigned char buf[8] = { 0 };
+	unsigned char oldbuf[8] = { 0 };
 
 	for (;;) {
 
 		int ret = libusb_interrupt_transfer(keyboard, endpoint_address, buf, sizeof(buf), &transferred, 0);
 
 		if (transferred == sizeof(buf)) {
-		char *err;
-		switch(ret) {
-			case LIBUSB_ERROR_PIPE:
-				err = "endpoint halted";
-				break;
-			case LIBUSB_ERROR_OVERFLOW:
-				err = "overflow";
-				break;
-			case LIBUSB_ERROR_NO_DEVICE:
-				err = "no device";
-				break;
-			case LIBUSB_ERROR_BUSY:
-				err = "busy";
-				break;
-			case LIBUSB_ERROR_TIMEOUT:
-				err = "timeout";
-				break;
-			case 0:
-				err = "no error";
-				break;
-			default:
-				err = "unknown error";
-				break;
-		}
+			char *err;
+			switch(ret) {
+				case LIBUSB_ERROR_PIPE:
+					err = "endpoint halted";
+					break;
+				case LIBUSB_ERROR_OVERFLOW:
+					err = "overflow";
+					break;
+				case LIBUSB_ERROR_NO_DEVICE:
+					err = "no device";
+					break;
+				case LIBUSB_ERROR_BUSY:
+					err = "busy";
+					break;
+				case LIBUSB_ERROR_TIMEOUT:
+					err = "timeout";
+					break;
+				case 0:
+					err = "no error";
+					break;
+				default:
+					err = "unknown error";
+					break;
+			}
 
-			printf("%s\n", buf);
 
-		
-		strcpy(oldbuf, buf);
+			if (memcmp(buf, oldbuf, sizeof(buf)) != 0) {
+				printf("%lx\n", *(long *)buf);
+				memcpy(oldbuf, buf, sizeof(buf));
+			}
 		}
 	}
-
-	return 0;
 }
