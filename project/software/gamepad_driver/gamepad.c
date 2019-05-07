3ef7ed56ea9791fc80b6ec1959a046c099a2ffb7
Daniel Mesko <dpmesko@gmail.com>
2019-05-07 21:18:31 +0000

    changed joystick driver to use SDL library, and not the far less practical libusb

--- project/software/gamepad_driver/gamepad.c
+++ project/software/gamepad_driver/gamepad.c
@@ -1,81 +1,85 @@
 #include <stdio.h>
-#include <linux/joystick.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "usbkeyboard.h"
+#include <SDL.h>
+#include <SDL_joystick.h>
+#include <SDL_events.h>
 
 
-struct libusb_device_handle *keyboard;
-uint8_t endpoint_address;
 
-struct usb_keyboard_packet packet;
-
-void using_libusb();
-void using_joystick();
+void using_sdl();
 
 
 int main()
 {
 
-//	using_joystick();
-
-	using_libusb();
+	using_sdl();
 	return 0;
 }
 
 
-void using_joystick()
+void using_sdl()
 {
-		printf("%s\n", JSIOCGVERSION);
-}
 
-
-
-void using_libusb()
-{
-	int transferred;
-
-	keyboard = openkeyboard(&endpoint_address);
-
-	unsigned char buf[8] = { 0 };
-	unsigned char oldbuf[8] = { 0 };
-
-	for (;;) {
-
-		int ret = libusb_interrupt_transfer(keyboard, endpoint_address, buf, sizeof(buf), &transferred, 0);
-
-		if (transferred == sizeof(buf)) {
-			char *err;
-			switch(ret) {
-				case LIBUSB_ERROR_PIPE:
-					err = "endpoint halted";
-					break;
-				case LIBUSB_ERROR_OVERFLOW:
-					err = "overflow";
-					break;
-				case LIBUSB_ERROR_NO_DEVICE:
-					err = "no device";
-					break;
-				case LIBUSB_ERROR_BUSY:
-					err = "busy";
-					break;
-				case LIBUSB_ERROR_TIMEOUT:
-					err = "timeout";
-					break;
-				case 0:
-					err = "no error";
-					break;
-				default:
-					err = "unknown error";
-					break;
-			}
-
-
-			if (memcmp(buf, oldbuf, sizeof(buf)) != 0) {
-				printf("%lx\n", *(long *)buf);
-				memcpy(oldbuf, buf, sizeof(buf));
-			}
-		}
+	SDL_Joystick *joystick1, *joystick2;
+	
+	if (SDL_Init(SDL_INIT_JOYSTICK) < 0)
+	{
+		fprintf(stderr, "Failed to initialize joystick via SDL: %s\n", SDL_GetError());
+		exit(1);
+	
 	}
+
+
+	int i, num_joy;
+	num_joy = SDL_NumJoysticks();
+
+//	if (num_joy != 2){
+//		fprintf(stderr, "One or more joysticks not found, please plug in both!");
+//		exit(1);
+//	}
+
+	for (i=0; i < num_joy; i++) {
+		fprintf(stderr, "joystick number: %d\nname: %s\n", i, SDL_JoystickName(i));
+	}
+
+	joystick1 = SDL_JoystickOpen(0);
+//	joystick2 = SDL_JoystickOpen(1);
+
+	int id1 = SDL_JoystickInstanceID(joystick1);
+	SDL_JoystickEventState(SDL_ENABLE);
+	SDL_Event event;
+
+	int x = 100;	
+	while(1){
+		
+		while (SDL_PollEvent(&event))
+		{
+			switch(event.type){
+				case SDL_JOYAXISMOTION:
+					printf("axis %d for joystick %d, value: %d\n", event.jaxis.axis, event.jaxis.which, event.jaxis.value);
+					break;
+				case SDL_JOYBUTTONDOWN:
+					printf("button %d pressed for joystick %d\n", event.jbutton.button, event.jbutton.which);
+					break;
+				case SDL_JOYBUTTONUP:
+					printf("button %d released for joystick %d\n", event.jbutton.button, event.jbutton.which);
+					break;
+				case 256:
+					fprintf(stderr, "SIGTERM caught, exiting...\n\n");
+					exit(0);
+				default:
+					printf("some other kind of event: %d\n", event.type);
+					break;
+			}
+			x--;
+
+		}
+		if (!x)
+			return;
+	}
+
+	SDL_JoystickClose(joystick1);
 }
+
