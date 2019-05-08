9793f9c9d5dd1eba03330568bb2124e5527abc4d
Daniel Mesko <dpmesko@gmail.com>
2019-05-08 21:31:45 +0000

    added struct for representing gamepad state, wait_event function now returns a pointer to one

--- project/software/gamepad_driver/gamepad.c
+++ project/software/gamepad_driver/gamepad.c
@@ -1,3 +1,12 @@
+// ***********************************************************************
+// gamepad.c
+//
+// 	Software to initialize, handle events from, and shutdown the gamepads
+//
+//
+// *************************************************************************
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,26 +17,58 @@
 
 #include "gamepad.h"
 
-// function signatures
-void init_gamepads();
-void shutdown_gamepads();
-void wait_event();
 
 
 // globals
 SDL_Joystick *joystick1, *joystick2;
+struct gamepad_state pad0, pad1;
 SDL_Event event;
 
 
-// dummy main for testing
+// dummy main function for testing
+/*
 int main()
 {
 	init_gamepads();
-	wait_event();
-	shutdown_gamepads();
+	int x = 100;
+	struct gamepad_state *p;
+	while (x--)
+	{
+		p = get_gamepad_event();
+
+		char msg[1000];
+		if (p->which == 0)
+			strcpy(msg, "Gamepad 0");
+		else
+			strcpy(msg, "Gamepad 1");
+
+		if (p->b0 == BUTTON_UP)
+			strcat(msg, "\nButton 0 up");
+		else
+			strcat(msg, "\nButton 0 down");
+
+		if (p->b1 == BUTTON_UP)
+			strcat(msg, "\nButton 1 up");
+		else
+			strcat(msg, "\nButton 1 down");
+
+		if (p->x == JOY_LEFT)
+			strcat(msg, "\nJoystick Left");
+		else if (p->x == JOY_RIGHT)
+			strcat(msg, "\nJoystick Right");
+
+		if (p->y == JOY_DOWN)
+			strcat(msg, "\nJoystick Down");
+		else if (p->y == JOY_UP)
+			strcat(msg, "\nJoystick Up");
+
+		printf("%s\n\n", msg);
+	}
 	return 0;
 }
+*/
 
+// initialize gamepads
 void init_gamepads()
 {
 	if (SDL_Init(SDL_INIT_JOYSTICK) < 0)
@@ -35,20 +76,32 @@ void init_gamepads()
 		fprintf(stderr, "Failed to initialize joystick via SDL: %s\n", SDL_GetError());
 		exit(1);
 	}
- 
- 	int num_joy;
+
+	int num_joy;
 	num_joy = SDL_NumJoysticks();
 
 	if (num_joy != 2){
-		fprintf(stderr, "One or more joysticks not found, please plug in both!");
+		fprintf(stderr, "One or more joysticks not found, please plug in both!\n");
 		exit(1);
 	}
-
 
 	joystick1 = SDL_JoystickOpen(0);
 	joystick2 = SDL_JoystickOpen(1);
 
 	SDL_JoystickEventState(SDL_ENABLE);
+	
+	pad0.which = 0;
+	pad0.b0 = BUTTON_UP;
+	pad0.b1 = BUTTON_UP;
+	pad0.x = JOY_CENTER;
+	pad0.y = JOY_CENTER;
+
+	pad1.which = 1;
+	pad1.b0 = BUTTON_UP;
+	pad1.b1 = BUTTON_UP;
+	pad1.x = JOY_CENTER;
+	pad1.y = JOY_CENTER; 
+	
 }
 
 void shutdown_gamepads()
@@ -57,36 +110,101 @@ void shutdown_gamepads()
 	SDL_JoystickClose(joystick2);
 }
 
-void wait_event()
+struct gamepad_state *get_gamepad_event()
 {
-	while(1){
-		
-		if (SDL_WaitEvent(&event))
-		{
-			switch(event.type){
-				case SDL_JOYAXISMOTION:
-					printf("axis %d for joystick %d, value: %d\n", event.jaxis.axis, event.jaxis.which, event.jaxis.value);
-					break;
-				case SDL_JOYBUTTONDOWN:
-					printf("button %d pressed for joystick %d\n", event.jbutton.button, event.jbutton.which);
-					break;
-				case SDL_JOYBUTTONUP:
-					printf("button %d released for joystick %d\n", event.jbutton.button, event.jbutton.which);
-					break;
-				case 256:
-					fprintf(stderr, "CTRL-C caught, exiting...\n\n");
-					return;
-				default:
-					printf("some other kind of event: %d\n", event.type);
-					break;
-			}
+	if (SDL_WaitEvent(&event))
+	{
+		switch(event.type){
+			case SDL_JOYAXISMOTION:
+//				printf("axis %d for joystick %d, value: %d\n", event.jaxis.axis, event.jaxis.which, event.jaxis.value);
+				if (event.jaxis.which == 0) {
+					if (event.jaxis.axis == 1) {
+						if (event.jaxis.value < 0) 
+							pad0.x = JOY_LEFT;
+						else if (event.jaxis.value == 0)
+							pad0.x = JOY_CENTER;
+						else
+							pad0.x = JOY_RIGHT;
+					}
+					else {
+						if (event.jaxis.value < 0)
+							pad0.y = JOY_DOWN;
+						else if (event.jaxis.value == 0)
+							pad0.y = JOY_CENTER;
+						else 
+							pad0.y = JOY_UP;
+					}
+					return &pad0;
+				}
+				else {
+					if (event.jaxis.axis == 1) {
+						if (event.jaxis.value < 0) 
+							pad1.x = JOY_LEFT;
+						else if (event.jaxis.value == 0)
+							pad1.x = JOY_CENTER;
+						else
+							pad1.x = JOY_RIGHT;
+					}
+					else {
+						if (event.jaxis.value < 0)
+							pad1.y = JOY_DOWN;
+						else if (event.jaxis.value == 0)
+							pad1.y = JOY_CENTER;
+						else 
+							pad1.y = JOY_UP;
+					}
+					return &pad1;
+				}
+				break;
+			case SDL_JOYBUTTONDOWN:
+				//printf("button %d pressed for joystick %d\n", event.jbutton.button, event.jbutton.which);
+				if (event.jbutton.which == 0) {
+					if (event.jbutton.button == 0)
+						pad0.b0 = BUTTON_DOWN;
+					else
+						pad0.b1 = BUTTON_DOWN;
+					return &pad0;
+				}
+				else {
+					if (event.jbutton.button == 0)
+						pad1.b0 = BUTTON_DOWN;
+					else
+						pad1.b1 = BUTTON_DOWN;
+					return &pad1;
+				}
+				break;
+			case SDL_JOYBUTTONUP:
+				//printf("button %d released for joystick %d\n", event.jbutton.button, event.jbutton.which);
+				if (event.jbutton.which == 0) {
+					if (event.jbutton.button == 0)
+						pad0.b0 = BUTTON_UP;
+					else
+						pad0.b1 = BUTTON_UP;
+					return &pad0;
+				}
+				else {
+					if (event.jbutton.button == 0)
+						pad1.b0 = BUTTON_UP;
+					else
+						pad1.b1 = BUTTON_UP;
+					return &pad1;
+				}
+
+				break;
+			case 256:
+				fprintf(stderr, "CTRL-C caught, exiting...\n\n");
+				shutdown_gamepads();
+				exit(0);
+			default:
+//				printf("some other kind of event: %d\n", event.type);
+				break;
 		}
-		else {
-			fprintf(stderr, "there was an error waiting for joystick event, exiting...\n");
-			exit(1);
-		}
+		return &pad0;
+	}
+	else {
+		fprintf(stderr, "there was an error waiting for joystick event, exiting...\n");
+		exit(1);
 	}
 
-	SDL_JoystickClose(joystick1);
 }
 
--- project/software/gamepad_driver/gamepad.h
+++ project/software/gamepad_driver/gamepad.h
@@ -2,9 +2,31 @@
 #define __GAMEPAD_H__
 
 
+// some #defines for gamepad state
+#define BUTTON_UP 0
+#define BUTTON_DOWN 1
+#define JOY_CENTER 0
+#define JOY_LEFT 1
+#define JOY_RIGHT 2
+#define JOY_UP 3
+#define JOY_DOWN 4
+
+
+// struct for representing gamepad state
+struct gamepad_state
+{
+	char which; // joystick 0 or 1
+	char b0;	// button 0
+	char b1;	// button 1
+	char x;		// X (L/R) axis of joystick
+	char y;		// Y (U/D) axis of joystick
+};
+
+
 // function signatures
 void init_gamepads();
 void shutdown_gamepads();
-void wait_event();
+struct gamepad_state *get_gamepad_event();
+
 
 #endif
