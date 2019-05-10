e61a46ebdcb7432733cef1144a5210337a9385cd
armerala <aa3938@columbia.edu>
2019-05-10 16:10:18 -0400

    renamed fpga top level

---
+++ project/hardware/fpga_top_level.sv
@@ -0,0 +1,14 @@
+
+module fpga_top_level(
+	input reset,
+	input clock,
+	input [7:0]  writedata,
+	input write,
+	input chipselect,
+	input [3:0]  address
+);
+
+	//vga_display vga_disp();
+	//sdram_controller sdram_ctrl();
+
+endmodule
