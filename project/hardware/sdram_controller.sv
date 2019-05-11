93aa2539ab99dcd3d18583bb0e97809634cb837a
armerala <aa3938@columbia.edu>
2019-05-11 19:29:24 -0400

    sdram controller compile errors fixed

--- project/hardware/sdram_controller.sv
+++ project/hardware/sdram_controller.sv
@@ -12,41 +12,50 @@
 //state for this module
 `define SDRAM_RESET_STATE 3'b000
 `define SDRAM_ACTIVATE_STATE 3'b001
-`define SDRAM_IDLE_STATE 3'b011
+`define SDRAM_WAIT_STATE 3'b011
 `define SDRAM_ISSUE_READ_STATE 3'b010
 `define SDRAM_DATA_READY_STATE 3'b110
 
-`define MODE_COMMAND \ 
+`define MODE_COMMAND \
 	mem_cs_n <= 1'b1; \
 	mem_ras_n <= 1'b0; \
 	mem_cas_n <= 1'b0; \
 	mem_we_n <= 1'b0; \
 	mem_ba <= 2'b00; \
 	mem_a[10] <= 1'b0; \
-	mem_a[9]  <= 1'b0; \   //burst mode
+	/*burst_mode*/ \
+	mem_a[9]  <= 1'b0; \
 	mem_a[8:7] <= 2'b00; \
-	mem_a[6:4] <= 3'b010;\ //cas latency 2
-	mem_a[3] <= 1'b0;    \ //sequntial access
-	mem_a[2:0] <= 3'b011;  //burst 8
+	/*cas latency 2*/\
+	mem_a[6:4] <= 3'b010; \
+	/*sequntial access*/ \
+	mem_a[3] <= 1'b0; \
+	/*burst 2*/ \
+	mem_a[2:0] <= 3'b011
 
 `define ACTIVATE_COMMAND(bank, row_addr) \
 	mem_cs_n <= 1'b0; \
 	mem_ras_n <= 1'b0; \
 	mem_cas_n <= 1'b1; \
 	mem_we_n <= 1'b1; \
-	mem_ba[1:0] <= bank; \     //bank
-	mem_a[12:0] <= row_addr; \ //row-address
+	/*bank*/ \
+	mem_ba[1:0] <= bank; \
+	/*row-addr*/ \
+	mem_a[12:0] <= row_addr
 
 `define NOP_COMMAND \
 	mem_cs_n <= 1'b0; \
 	mem_ras_n <= 1'b1; \
 	mem_cas_n <= 1'b1; \
-	mem_we_n <= 1'b1;
+	mem_we_n <= 1'b1
 
-`define READ_COMMAND(bank, start_col_addr) \
-	mem_ba[1:0] <= bank; \           //bank
-	mem_a[9:0] <= start_col_addr; \  //set starting col address
-	mem_a[10] <= 1'b1; \             //set auto-precharge (we don't need to read more than once)
+`define ISSUE_READ_COMMAND(bank, start_col_addr) \
+	/*bank*/ \
+	mem_ba[1:0] <= bank; \
+	/*starting col addr*/ \
+	mem_a[9:0] <= start_col_addr; \
+	/*auto pre-charge (we dont need to read a region more than once)*/ \
+	mem_a[10] <= 1'b1
 
 /************************************************************************************************
  **************************************MODULE BEGINS HERE****************************************
@@ -68,7 +77,7 @@ module sdram_controller(
 	input reset_n,                //reset signal -negative
 	input pause,                  //pause (e.g. if output buffer is full)
 	input unpause,                //unpause signal
-	output reg data_available;    //amount of data available
+	output reg data_available     //amount of data available
 );
 
 
@@ -77,6 +86,7 @@ reg [31:0] img_load_counter;
 reg [3:0] state;
 reg [3:0] next_state;
 reg [1:0] n_data_available; //2 read burst
+reg is_paused;
 
 //start cke high
 initial begin
@@ -85,10 +95,9 @@ initial begin
 	mem_udqm <= 1'b0;
 	mem_a <= 13'b0;
 	mem_ba <= 2'b0;
-	`NOP_COMMAND
+	`NOP_COMMAND;
 
-	dq <= 16'b0;
-	next_state <= `RESET_STATE;
+	next_state <= `SDRAM_RESET_STATE;
 end
 
 /**
@@ -97,7 +106,7 @@ end
 always @(negedge reset_n, posedge pause, posedge unpause)
 begin
 	if(~reset_n)
-		next_state <= `RESET_STATE;
+		next_state <= `SDRAM_RESET_STATE;
 
 	if(pause)
 		is_paused <= 1'b1;
@@ -115,12 +124,11 @@ always @(negedge ck143)
 begin
 
 	case(state)
-	begin
 
 		//case : reset
 		`SDRAM_RESET_STATE: begin
 
-			`MODE_COMMAND
+			`MODE_COMMAND;
 			img_load_counter <= 32'b0;
 			n_data_available <= 2'b0;
 			next_state <= `SDRAM_ACTIVATE_STATE;
@@ -128,32 +136,33 @@ begin
 		
 		//case: issue activate command
 		`SDRAM_ACTIVATE_STATE : begin 
-			`ACTIVATE_COMMAND(1'b1,1'b1)
-			next_state <= `ISSUE_READ_STATE;
+			`ACTIVATE_COMMAND(1'b1,1'b1);
+			next_state <= `SDRAM_ISSUE_READ_STATE;
 		end
 
 		//case: issue read -- only allow arrest before issuing read
 		`SDRAM_ISSUE_READ_STATE : begin 
 			if(~is_paused) begin
-				`READ_COMMAND(1'b1, 1'b1) 
+				`ISSUE_READ_COMMAND(1'b1, 1'b1);
 				next_state <= `SDRAM_WAIT_STATE;
 			end
-			else
-				`NOP_COMMAND
+			else begin
+				`NOP_COMMAND;
+			end
 		end
 	
 		//case : wait one until data is ready
 		`SDRAM_WAIT_STATE : begin
-			`NOP_COMMAND
+			`NOP_COMMAND;
 			next_state = `SDRAM_DATA_READY_STATE;
 		end
 
 		//case: starting spitting data and raise proper flag
-		`SDRAM_DATA_READY_STEP : begin 
-			`NOP_COMMAND 
+		`SDRAM_DATA_READY_STATE : begin 
+			`NOP_COMMAND;
 			n_data_available <= 2'b11;
 			data_available <= 1'b1;
-			next_state <= SDRAM_ACTIVATE_STATE;
+			next_state <= `SDRAM_ACTIVATE_STATE;
 		end
 
 	endcase
@@ -177,6 +186,7 @@ begin
 	//Next neg edge data_available will be 0 then
 	data_available <= (data_available > 2'b01) ? 1'b1 : 1'b0; 
 
+end
+
+
 endmodule
-
-
